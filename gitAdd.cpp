a4660f58b2cff8d2c2374ec9be0530fccf52808a
Pranav Verma <pranaverma27@gmail.com>
2018-11-23 19:54:28 +0530

    gitAdd changed

--- gitAdd.cpp
+++ gitAdd.cpp
@@ -1,1 +1,30 @@
 #include<stdio.h>
+#include<string>
+#include <openssl/sha.h>
+#include "generateSHA.h"
+// #include<boost.h>
+
+void gitAdd(string fileName){
+    FILE *textFile,*binFile;
+    char *buffer;
+    textFile=fopen(fileName.c_str(),"r");
+    binFile=fopen("binaryFile.bin","wb");
+    while (fgetc(textFile) != EOF)
+    {
+        fread(buffer, 1, 1, textFile);
+        fwrite(buffer, 1, 1, binFile);
+    }
+
+    string hashString=generateSHA("binaryFile.bin");
+    cout<<hashString<<endl;
+
+    fclose(textFile);
+    fclose(binFile);   
+
+}
+
+int main(){
+    string fileName="abc.txt";
+    gitAdd(fileName);
+    return 0;
+}
